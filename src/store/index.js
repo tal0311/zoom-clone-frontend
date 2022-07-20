5811b467b7ac0c1b836e039bdb5dcc0a5d777c38
tal0311 <tal.amit0311@gmail.com>
2022-07-20 22:01:09 +0300

    vuex and clen up

---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+// Create a new store instance.
+const store = createStore({
+  state: {
+    msg: 'store running',
+  },
+  getters: {},
+  mutations: {},
+  actions: {},
+})
+
+export default store
