c814518e66f11a17bf8868719a6ba9d653ccc024
tal0311 <tal.amit0311@gmail.com>
2022-07-21 23:36:32 +0300

    icons - meeting

--- src/views/zoom-meeting.vue
+++ src/views/zoom-meeting.vue
@@ -1,5 +1,14 @@
 <template>
   <i class="fa-solid fa-microphone"></i>
+  <i class="fa-solid fa-video"></i>
+  <i class="fa-solid fa-shield-halved"></i>
+  <i class="fa-solid fa-user-group"></i>
+  <i class="fa-solid fa-desktop"></i>
+  <i class="fa-solid fa-face-smile"></i>
+  <i class="fa-solid fa-arrow-up-right-from-square"></i>
+  <i class="fa-solid fa-vector-square"></i>
+  <i class="fa-solid fa-chalkboard"></i>
+  <i class="fa-solid fa-record-vinyl"></i>
 </template>
 
 <script>
