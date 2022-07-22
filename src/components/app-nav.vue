2e534b371887fca1eab554b33b715494f0010fe4
tal0311 <tal.amit0311@gmail.com>
2022-07-22 19:16:28 +0300

    user-preview cmp

--- src/components/app-nav.vue
+++ src/components/app-nav.vue
@@ -2,18 +2,18 @@
   <nav class="app-nav flex space-between align-center">
     <div class="filter-container">filter</div>
     <nav-actions></nav-actions>
-    <div class="user-preview flex">
-      <p>user</p>
-    </div>
+    <user-preview></user-preview>
   </nav>
 </template>
 
 <script>
 import navActions from './nav-actions.vue'
+import userPreview from './user-preview.vue'
 export default {
   name: 'app-nav',
   components: {
     navActions,
+    userPreview,
   },
 }
 </script>
---
+++ src/components/user-preview.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="user-preview flex">
+    <img src="./../assets/images/tal-profile.jpg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'user-preview',
+}
+</script>
+
+<style></style>
