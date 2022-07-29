a9e1efed78795ca97c337139ede93ad51db8b7cb
tal0311 <tal.amit0311@gmail.com>
2022-07-29 23:54:15 +0300

    store set

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,56 @@ import { createStore } from 'vuex'
 const store = createStore({
   state: {
     msg: 'store running',
+    stream: null,
+    msgs: [],
+    users: [],
+    currMeeting: '',
+    userMsg: null,
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    getStream(state) {
+      return state.stream
+    },
+    getMsgs(state) {
+      return state.msgs
+    },
+    getUsers(state) {
+      return state.users
+    },
+    getMeeting(state) {
+      return state.meeting
+    },
+  },
+  mutations: {
+    removeMsg({ msgs }, msgId) {
+      const idx = msgs.findIndex((msg) => msg._id === msgId)
+      msgs.splice(idx, 1)
+    },
+    addMsg({ msgs }, msg) {
+      msgs = [...msgs, msg]
+    },
+    removeUserFromMeeting({ users }, userId) {
+      const idx = users.findIndex((user) => user._id === userId)
+      users.splice(idx, 1)
+    },
+    setUserMsg({ userMsg }, msg) {
+      userMsg = msg
+    },
+    setUserStream({ stream }, localStream) {
+      stream = localStream
+    },
+  },
+  actions: {
+    async loadUsers({ commit }) {
+      try {
+      } catch (error) {
+        commit({ type: 'setUserMsg', msg })
+      }
+    },
+    loadStreams({ commit, state }) {
+      commit.set
+    },
+  },
 })
 
 export default store
