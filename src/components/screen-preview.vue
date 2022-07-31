4fd1d4190794d42f19e6dd310fa036ef8e52c404
tal0311 <tal.amit0311@gmail.com>
2022-07-31 23:18:38 +0300

    /

--- src/components/screen-preview.vue
+++ src/components/screen-preview.vue
@@ -1,9 +1,9 @@
 <template>
-  <section class="grid-item">
+  <section class="screen-preview grid-item">
     <div class="info-container flex align-center">
-      <p>user name</p>
-      <button>mute</button>
-      <button>
+      <p class="user-title">user name</p>
+      <button class="mute-btn-screen">mute</button>
+      <button class="menu-btn-screen">
         <i class="fa-solid fa-ellipsis-vertical"></i>
       </button>
     </div>
