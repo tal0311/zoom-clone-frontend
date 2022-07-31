eba5d8eba4d42ef5b97190ae05ab64b6eda0f169
tal0311 <tal.amit0311@gmail.com>
2022-07-31 19:15:28 +0300

    screen list/preview

--- src/components/screen-list.vue
+++ src/components/screen-list.vue
@@ -1,18 +1,41 @@
 <template>
   <div class="screens grid-container">
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
+    <screen-preview v-for="screen in screens" :key="screen._id">
+      ></screen-preview
+    >
+
+    <!--  -->
   </div>
 </template>
 
 <script>
+import screenPreview from './screen-preview.vue'
 export default {
   name: 'screens-list',
+  data() {
+    return {
+      screens: [
+        {
+          _id: '101',
+        },
+        {
+          _id: '102',
+        },
+        {
+          _id: '103',
+        },
+        {
+          _id: '104',
+        },
+        {
+          _id: '105',
+        },
+      ],
+    }
+  },
+  components: {
+    screenPreview,
+  },
 }
 </script>
 
---
+++ src/components/screen-preview.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="grid-item">
+    <h4>user name</h4>
+    <button>opt</button>
+    <button>mute</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'screen-preview',
+}
+</script>
+
+<style></style>
