b3e21cb9c7213dd394031cf53c18f43b57920436
tal0311 <tal.amit0311@gmail.com>
2022-10-15 12:39:43 +0300

    render home btns

--- src/components/home-actions.vue
+++ src/components/home-actions.vue
@@ -1,25 +1,81 @@
 <template>
   <section class="home-actions-container flex">
     <button
-      @click="onNewMeeting('meeting/123')"
-      class="action fa-solid fa-video"
+      v-for="(button, idx) in buttons"
+      :key="idx"
+      :title="button.title"
+      :class="button.icon"
+      @click="onAction(button.value)"
     ></button>
-    <button class="action fa-solid fa-plus"></button>
-    <button class="action fa-solid fa-calendar-days"></button>
-    <button class="action fa-solid fa-share-nodes"></button>
   </section>
 </template>
 
 <script>
 export default {
   name: 'home-actions',
+  data() {
+    return {
+      buttons: [
+        {
+          icon: 'action fa-solid fa-video',
+          title: 'Start new meeting',
+          value: 'new-meeting',
+        },
+        {
+          icon: 'action fa-solid fa-plus',
+          title: 'Join meeting',
+          value: 'join',
+        },
+        {
+          icon: 'action fa-solid fa-calendar-days',
+          title: 'Schedule',
+          value: 'schedule',
+        },
+        {
+          icon: 'action fa-solid fa-share-nodes',
+          title: 'Share meeting',
+          value: 'share',
+        },
+      ],
+    }
+  },
 
   methods: {
-    onNewMeeting(path) {
+    onAction(action) {
+      switch (action) {
+        case 'new-meeting':
+          this.onNewMeeting()
+          break
+        case 'schedule':
+          this.onSchedule()
+          break
+        case 'join':
+          this.onJoin()
+          break
+        case 'share':
+          this.onShare()
+          break
+
+        default:
+          break
+      }
+    },
+
+    onNewMeeting() {
+      const path = 'meeting/123'
       const { width, height, left, top } = this.$store.getters.getUserSettings
-      const windowFeatures = `location=no,toolbar=no,menubar=no,height=${height}
+      const windowFeatures = `height=${height}
       ,width=${width},left=${left},top=${top}`
       window.open(document.URL + path, '_blank', windowFeatures)
+    },
+    onSchedule() {
+      console.log('Schedule meeting')
+    },
+    onJoin() {
+      console.log('onJoin meeting')
+    },
+    onShare() {
+      console.log(' onShare meeting')
     },
   },
 }
